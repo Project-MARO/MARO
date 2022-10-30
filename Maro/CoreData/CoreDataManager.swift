52ae9b2c57dfb2fd8a3b60f2bdd2902c42330e30
insub4067 <vb15@kakao.com>
2022-10-30 20:40:47 +0900

    feat : AppGroup 제거

--- Maro/CoreData/CoreDataManager.swift
+++ Maro/CoreData/CoreDataManager.swift
@@ -9,62 +9,20 @@ import CoreData
 
 final class CoreDataManager {
     static let shared = CoreDataManager()
-    private let container = NSPersistentContainer(name: "DataModel")
-    private let cloudContainer = NSPersistentCloudKitContainer(name: "DataModel")
-    private let databaseName = "DataModel.sqlite"
+    private let container = NSPersistentContainer(name: CoreData.container)
     private var context: NSManagedObjectContext {
         container.viewContext
     }
 
-    var oldStoreURL: URL {
-        let directory = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
-        return directory.appendingPathComponent(databaseName)
+    private init() {
+        loadStores()
     }
 
-    var sharedStoreURL: URL {
-        let container = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.kim.Maro")!
-        return container.appendingPathComponent(databaseName)
-    }
-
-    init() {
-        print("core data init")
-
-        if !FileManager.default.fileExists(atPath: oldStoreURL.path) {
-            print("old store doesn't exist. Using new shared URL")
-            container.persistentStoreDescriptions.first!.url = sharedStoreURL
-        }
-
-        print("Container URL = \(container.persistentStoreDescriptions.first!.url!)")
-
+    private func loadStores() {
         container.loadPersistentStores { desc, error in
-            if let error = error {
-                print(error.localizedDescription)
-            }
-        }
-
-        migrateStore(for: container)
-        container.viewContext.automaticallyMergesChangesFromParent = true
-    }
-
-    func migrateStore(for container: NSPersistentContainer) {
-        print("Went into MigrateStore")
-        let coordinator = container.persistentStoreCoordinator
-
-        guard let oldStore = coordinator.persistentStore(for: oldStoreURL) else { return }
-        print("old store no longer exists")
-
-        do {
-            let _ = try coordinator.migratePersistentStore(oldStore, to: sharedStoreURL, type: .sqlite)
-            print("Migration Succeed")
-        } catch {
-            fatalError("Unable to migrate to shared store")
-        }
-
-        do {
-            try FileManager.default.removeItem(at: oldStoreURL)
-            print("Old Store Deleted")
-        } catch {
-            print("Unable to delete oldStore")
+              if let error = error {
+                  print(error.localizedDescription)
+              }
         }
     }
 }
